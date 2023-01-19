b63a7b9b7c8e11dd3de8f581e7d05f8693980ed9
dach83 <dach83@mail.ru>
2023-01-19 15:48:16 +0300

    Successful load excel file updates ui state to loaded

--- app/src/main/java/com/github/dach83/gasmetering/features/abonents/presentation/AbonentsViewModel.kt
+++ app/src/main/java/com/github/dach83/gasmetering/features/abonents/presentation/AbonentsViewModel.kt
@@ -5,15 +5,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AbonentsViewModel @Inject constructor() : ViewModel() {
+
     var uiState by mutableStateOf(AbonentsUiState.INITIAL)
         private set
 
+    private var loadingJob: Job? = null
+
     fun loadExcelFile(excelUri: Uri) {
         uiState = uiState.loading()
+
+        loadingJob?.cancel()
+        loadingJob = viewModelScope.launch {
+            uiState = uiState.loaded()
+        }
     }
 }
--- app/src/test/java/com/github/dach83/gasmetering/features/abonents/presentation/AbonentsViewModelTest.kt
+++ app/src/test/java/com/github/dach83/gasmetering/features/abonents/presentation/AbonentsViewModelTest.kt
@@ -1,11 +1,20 @@
 package com.github.dach83.gasmetering.features.abonents.presentation
 
 import android.net.Uri
+import com.github.dach83.gasmetering.rule.CoroutineRule
 import com.google.common.truth.Truth.assertThat
 import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class AbonentsViewModelTest {
+
+    @get:Rule
+    val coroutineRule = CoroutineRule()
 
     private val excelUri: Uri = mockk()
 
@@ -25,6 +34,19 @@ class AbonentsViewModelTest {
         )
 
         sut.loadExcelFile(excelUri)
+
+        assertThat(sut.uiState).isEqualTo(expected)
+    }
+
+    @Test
+    fun `successful load excel file updates ui state to loaded`() = runTest {
+        val sut = AbonentsViewModel()
+        val expected = AbonentsUiState(
+            isLoading = false
+        )
+
+        sut.loadExcelFile(excelUri)
+        advanceUntilIdle()
 
         assertThat(sut.uiState).isEqualTo(expected)
     }
